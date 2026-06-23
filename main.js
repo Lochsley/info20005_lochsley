6c95158fa244abe50725d7621278db83288b58b7
Lochsley <lochsleyw@student.unimelb.edu.au>
2026-06-23 15:37:51 +1000

    Mobile nav interaction (".hidden" toggles for accordion list items) finalised

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-// Defines variables for hamburger button and mobile menu div
+//Hamburger menu interactions (opening/closing mobile menu overlay; opening/closing accordion menu)
+
+// Defines variables for hamburger button and mobile menu div.
     const hamburgerBtn = document.getElementById('hamburger-btn');
     const mobileMenu = document.getElementById('mobile-menu');
 
-// Shows or hides mobile menu when hamburger button is clicked (using "toggle" method)
+// Shows or hides mobile menu when hamburger button is clicked (using "toggle" method).
     if (hamburgerBtn && mobileMenu) {
         hamburgerBtn.addEventListener('click', () => {
             mobileMenu.classList.toggle('hidden');
+        });
+    }
+
+// Defines variables for accordion button/list children (sub menu).
+    const accordionBtn = document.querySelector('.accordion-btn');
+    const subMenu = document.querySelector('.sub-menu')
+
+// Show or hides submenu items when parent is clicked (also using "toggle")
+    if (accordionBtn && subMenu) {
+        accordionBtn.addEventListener('click', () => {
+            subMenu.classList.toggle('hidden');
         });
     }
 });
