492861f853ada9cc94eec54c16f9bc478bd8dbb5
Lochsley <lochsleyw@student.unimelb.edu.au>
2026-06-23 17:04:14 +1000

    Adding cart overlay (inc. open/close interactions), updating button classes to use BEM method

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-//Hamburger menu interactions (opening/closing mobile menu overlay; opening/closing accordion menu)
+// HAMBURGER MENU (mobile nav)
 
 // Defines variables for hamburger button and mobile menu div.
     const hamburgerBtn = document.getElementById('hamburger-btn');
@@ -23,4 +23,26 @@ document.addEventListener('DOMContentLoaded', () => {
             subMenu.classList.toggle('hidden');
         });
     }
+
+// CART OVERLAY ('Your Bag')
+
+// Defines variables for bag button, cart overlay div, close cart button
+    const bagBtn = document.getElementById('bag-btn')
+    const cartOverlay = document.getElementById('cart-overlay');
+    const closeCartBtn = document.getElementById('close-cart-btn');
+
+// Shows or hides cart overlay when bag button is clicked (using "toggle" method)
+    if (bagBtn && cartOverlay) {
+    bagBtn.addEventListener('click', () => {
+        cartOverlay.classList.toggle('hidden');
+    });
+    
+// Hides the cart overlay when the close cart button is clicked (also using "toggle")
+    if (closeCartBtn) {
+        closeCartBtn.addEventListener('click', () => {
+            cartOverlay.classList.add('hidden');
+        });
+    }
+}
+
 });
